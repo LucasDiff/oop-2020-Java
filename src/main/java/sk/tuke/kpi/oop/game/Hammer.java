2608791a38858ccf2ad327b4c2bb3f2521b94b88
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-11-01 04:29:47 +0100

    Add new file

---
+++ src/main/java/sk/tuke/kpi/oop/game/Hammer.java
@@ -0,0 +1,42 @@
+package sk.tuke.kpi.oop.game.items;
+
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+import sk.tuke.kpi.gamelib.graphics.Point;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import sk.tuke.kpi.gamelib.Actor;
+
+
+public class Hammer {
+    private Animation normalAnimation;
+
+
+
+    public Hammer() {
+        setAnimation(new Animation(
+            "sprites/hammer.png",
+            16,
+            16,
+            0.1f,
+            Animation.PlayMode.ONCE
+        ));
+    }
+
+    private void setAnimation(Animation normalAnimation) {
+        this.normalAnimation = normalAnimation;
+    }
+
+    public Hammer(int usesLeft) {
+
+        setAnimation(new Animation(
+            "sprites/hammer.png",
+            16,
+            16,
+            0.1f,
+            Animation.PlayMode.ONCE
+        ));
+    }
+
+
+}
