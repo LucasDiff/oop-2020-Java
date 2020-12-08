f5a2848e6e85ae1377a13f307102bb5ab88eb2a1
Lukas Nemeth <lukas.nemeth.@student.tuke.sk>
2020-12-08 19:52:32 +0100

    commit

---
+++ src/main/resources/maps/tilesets/ab_tiles_a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ab_tiles_a" tilewidth="16" tileheight="16" tilecount="240" columns="16">
+ <image source="ab_tiles_a.png" width="256" height="240"/>
+</tileset>
